4d1a58ef8ad1189ade6b9258438b5bca9d7b8eff
Chris <lichliter@ymail.com>
2020-04-08 15:32:45 -0500

    Completed PyBank

--- Submissions/3_Python/PyBank/main.py
+++ Submissions/3_Python/PyBank/main.py
@@ -1,14 +1,11 @@
 #Read and open budget data
 import csv
 
-csvpath = r"PyBank\Resources\budget_data.csv"
-
 with open(r"PyBank\Resources\budget_data.csv") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     
     #remove header
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
 
     #create dictionaries to reference for calcs
     month = []
@@ -20,40 +17,47 @@ with open(r"PyBank\Resources\budget_data.csv") as csvfile:
     
     #calc total months
     tmonths = len(month)
-    print(tmonths)
 
-    #calc total profits, tmonths is also the end value of the range
-    #define variable
+#calc total profits, tmonths is also the end value of the range
+    #starting variable values
     tprofit = 0
-
     #loop to add profits
-    for i in range(0, tmonths):
-        tprofit += int(profit[i])
-        currentprofit = int(profit[i])
+    for _ in range(0, tmonths):
+        tprofit += int(profit[_])
+        currentprofit = int(profit[_])
     #loop to calc and store profit change    
-        lastprofit = int(profit[i - 1])
+        lastprofit = int(profit[_ - 1])
         profitchange.append(currentprofit - lastprofit)
-    print(tprofit)
 
-    #avg profit change, loop to total changes, divide by tmonths -1.
+#avg profit change, loop to total changes, divide by (tmonths -1).
     tchange = 0
-    for i in range(1, tmonths):
-        tchange += int(profitchange[i])
+    for _ in range(1, tmonths):
+        tchange += int(profitchange[_])
     avgchange = tchange / (tmonths - 1)
-    print(len(profitchange))
-    print(avgchange)
 
-
-    #find greatest monthly increase and decrease
+#find greatest monthly increase and decrease.
     increase = max(profitchange)
-    print(increase)
     decrease = min(profitchange)
-    print(decrease)
+
+#find month with index
     increasemonth = month[(profitchange.index(increase))]
     decreasemonth = month[(profitchange.index(decrease))]
-    print(increasemonth)
-    print(decreasemonth)
 
-    #print calcs to terminal
+#print calcs to terminal
+    print("Financial Analysis")
+    print("***********************************************")
+    print(f"Total Months: {tmonths}")
+    print(f"Total Profit: {tprofit}")
+    print(f"Average Monthly Change: ${avgchange}")
+    print(f"Greatest Monthly Increase: {increasemonth} $({increase})")
+    print(f"Greatest Monthly Decrease: {decreasemonth} $({decrease})")
 
-# #write calcs to .txt
+#write calcs to .txt
+outputtxt = open(r"PyBank\Financials.txt", "w")
+outputtxt.write("Financial Analysis \n")
+outputtxt.write("*********************************************** \n")
+outputtxt.write(f"Total Months: {tmonths} \n")
+outputtxt.write(f"Total Profit: {tprofit} \n")
+outputtxt.write(f"Average Monthly Change: ${avgchange} \n")
+outputtxt.write(f"Greatest Monthly Increase: {increasemonth} $({increase}) \n")
+outputtxt.write(f"Greatest Monthly Decrease: {decreasemonth} $({decrease}) \n")
