c7c52f3b73a8cafb3223a2408e43ea1ed0f567a9
Chris <lichliter@ymail.com>
2020-06-30 12:51:07 -0500

    Completed draft of app.py. Need to write html

---
+++ Submissions/12_WebScraping/html/apps/app.py
@@ -0,0 +1,39 @@
+#import dependencies and scrapping app
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import martian_scrape
+
+app = Flask(__name__)
+
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/mars_app'
+monto = PyMongo(app)
+
+#homepage, intial data
+@app.route('/')
+def index():
+    mars = mongo.db.mars.find_one{'active': 1})
+    return render_template('index.html', mars=mars)
+
+#perform on button click
+
+@app.route('/scrape')
+def scrape():
+    mars_app = mongo.db.mars
+    mars = martian_scrape.mars_attacks()
+
+    #Change previous entry to inactive
+
+    mars_app.update_many(
+        {'active': 1},
+        {"$set": {'active': 0}
+        }
+    )
+
+    #insert new data
+    mars_app.instert_one(mars)
+
+    #redirect to index
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run
